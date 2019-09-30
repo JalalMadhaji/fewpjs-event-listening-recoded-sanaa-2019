a9a84e25986206d4fa39a5e4a3a1c3abb916a60e
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-09-30 19:25:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addingEventListener(par,ev) {
+  par.addEventListener(ev,function(event) {
+  alert('I was clicked!');
+});
 }
+
+let input1 = document.querySelector('input #input');
+addingEventListener(input1,'click');
